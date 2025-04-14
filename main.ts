4ea8040fee493a1431924e9827253b178b51271f
Sangrak Choi <kars@kargn.as>
2025-04-15 02:40:28 +0900

    기능 추가

--- main.ts
+++ main.ts
@@ -1,134 +1,99 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, TFile, TAbstractFile } from 'obsidian';
+import * as moment from 'moment';
+// Import JSON files directly
+import koTranslations from './i18n/ko.json';
+import enTranslations from './i18n/en.json';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface Translations {
+	COMMANDS: {
+		CREATE_TODAY_NOTE: string;
+	};
+	NOTICES: {
+		FILE_EXISTS: string;
+		FILE_CREATED: string;
+		FOLDER_NOT_FOUND: string;
+		ERROR_CREATING_FILE: string;
+	};
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export default class KarsPlugin extends Plugin {
+	getText(key: string, vars: Record<string, string> = {}): string {
+		// Get language setting from localStorage
+		const language = window.localStorage.getItem('language') || 'en';
+		const translations = language === 'ko' ? koTranslations : enTranslations;
+		console.log(language);
+		
+		const keys = key.split('.');
+		let value: any = translations;
+		for (const k of keys) {
+			if (!value || !value[k]) return key;
+			value = value[k];
+		}
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+		if (typeof value !== 'string') return key;
+
+		return value.replace(/{([^}]+)}/g, (_: string, name: string) => vars[name] || `{${name}}`);
+	}
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// Add command for creating today's note
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: 'create-today-note',
+			name: this.getText('COMMANDS.CREATE_TODAY_NOTE'),
 			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
+				const activeFile = this.app.workspace.getActiveFile();
+				if (activeFile) {
 					if (!checking) {
-						new SampleModal(this.app).open();
+						this.createTodayNote(activeFile);
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
+				return false;
 			}
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-
+		// Cleanup when plugin is disabled
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+	// Function to create a new note with today's date
+	async createTodayNote(activeFile: TFile) {
+		const today = new Date();
+		const year = today.getFullYear();
+		const month = String(today.getMonth() + 1).padStart(2, '0');
+		const day = String(today.getDate()).padStart(2, '0');
+		const formattedDate = `${year}-${month}-${day}`;
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+		// Get parent folder path of the current file
+		const parentFolder = activeFile.parent;
+		if (!parentFolder) {
+			new Notice(this.getText('NOTICES.FOLDER_NOT_FOUND'));
+			return;
+		}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+		// Create new file path
+		const newFilePath = `${parentFolder.path}/${formattedDate}.md`;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+		try {
+			// Check if file already exists
+			const existingFile = this.app.vault.getAbstractFileByPath(newFilePath);
+			if (existingFile) {
+				new Notice(this.getText('NOTICES.FILE_EXISTS', { filename: `${formattedDate}.md` }));
+				return;
+			}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+			// Create new file
+			await this.app.vault.create(newFilePath, '');
+			new Notice(this.getText('NOTICES.FILE_CREATED', { filename: `${formattedDate}.md` }));
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			// Open the newly created file
+			const newFile = this.app.vault.getAbstractFileByPath(newFilePath);
+			if (newFile instanceof TFile) {
+				await this.app.workspace.getLeaf().openFile(newFile);
+			}
+		} catch (error) {
+			new Notice(this.getText('NOTICES.ERROR_CREATING_FILE', { error: error.message }));
+		}
 	}
 }
